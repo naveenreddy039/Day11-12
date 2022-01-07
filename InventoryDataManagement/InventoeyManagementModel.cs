045affa2926b304a7fa6b479cca34ba11d17178c
naveen <naveenreddy.k07@gmail.com>
2022-01-07 13:10:25 +0530

    InventoryManagement

---
+++ InventoryDataManagement/InventoeyManagementModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryDataManagement
+{
+    public class InventoeyManagementModel
+    {
+        public List<Rice> Rice;
+        public List<Pulses> Pulses;
+        public List<Wheat> Wheat;
+    }
+    public class Rice
+    {
+        public String name;
+        public int weight;
+        public int price;
+    }
+    public class Pulses
+    {
+        public String name;
+        public int weight;
+        public int price;
+    }
+    public class Wheat
+    {
+        public String name;
+        public int weight;
+        public int price;
+    }
+}
---
+++ InventoryDataManagement/ReadInventoryData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace InventoryDataManagement
+{
+    public class ReadInventoryData
+    {
+        public InventoeyManagementModel Read(String Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    String json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoeyManagementModel>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("problem in getting data {0}", e);
+                    return null;
+                }
+            }
+        }
+    }
+}
